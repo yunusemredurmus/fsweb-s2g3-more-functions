d337f335c6a0cf3369569d649332973e5e3788a9
yunusemredurmus <128366167+yunusemredurmus@users.noreply.github.com>
2023-03-30 00:44:52 +0300

    S2G3

--- index.js
+++ index.js
@@ -16,10 +16,13 @@
   örnek output: ""
 */
 
-function dosyaAdiniBul(/* kodlar buraya */) {
-  // kodlar buraya
+function dosyaAdiniBul(path) {
+  const ayirma = path.split("/");
+  const dosyaAdi = ayirma[ayirma.length - 1];
+  return dosyaAdi
 }
 
+console.log(dosyaAdiniBul("C:/Users/PC/Desktop/1x"));
 /*
   GÖREV 2
   - Input:  Bir sayı arrayini parametre olarak alacak bir fonksiyon oluştur.
@@ -38,10 +41,25 @@ function dosyaAdiniBul(/* kodlar buraya */) {
   örnek output: 104
 */
 
-function ortalamaBul(/* kodlar buraya */) {
-  // kodlar buraya
-}
+const numbers2 = [109, 216, 288, 143, 71, 185, -278, 194, 5];
+function ortalamaBul(numbers) {
+  const sumNumbers = numbers.reduce((sayi, toplam) => sayi + toplam, 0);
+  const ortNumbers = sumNumbers / numbers.length;
+  const ortNumbers1 = Math.ceil(ortNumbers);
+  return ortNumbers1;
+} console.log(ortalamaBul(numbers2))
 
+// const numbers2 = [109, 216, 288, 143, 71, 185, -278, 194, 5];
+// function ortalamaBul(numbers) {
+//   let toplam = 0;
+//   for (let i = 0; i < numbers2.length; i++) {
+//     toplam += numbers2[i];
+//   }
+//   let ortalama0 = toplam / numbers.length;
+
+//   return Math.ceil(ortalama0);
+// }
+// console.log(ortalamaBul(numbers2));
 /*
   GÖREV 3
   - Input:  Bir sayı arrayini ve ortalama bulmaya yarayacak bir fonksiyonu parametre olarak alacak bir fonksiyon oluştur.
@@ -61,10 +79,25 @@ function ortalamaBul(/* kodlar buraya */) {
   örnek input:  [109, 216, 288, 143, 71, 185, -278, 194, 5]
   örnek output: [109, 216, 288, 143, 185, 194]
 */
+const sayiArry = [50, -26, 153, 7]
+function ortalamadanBuyukleriBul(sdize, callback) {
+  const ortalama1 = callback(sdize);
+  // const newSayi2 = sdize.filter((sayi) => sayi >= ortalama1);
 
-function ortalamadanBuyukleriBul(/* kodlar buraya */) {
-  // kodlar buraya
+  // for (let i = 0; i < sdize.length; i++) {
+  //   if (sdize[i] >= ortalama1) {
+  //     newSayi2.push(sdize[i])
+  //   }
+  // }
+  return sdize.filter((sayi) => sayi >= ortalama1);
 }
+console.log(ortalamadanBuyukleriBul(sayiArry, ortalamaBul));
+
+
+
+
+
+
 
 /* !!!! Burdan aşağısını değiştirmeyin !!!! */
 function as() {
